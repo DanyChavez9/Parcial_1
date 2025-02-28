ca7d52a8ebcd77d0a3c962cb706fba216375d97b
ALUMNOPC10 <ALUMNOPC10@LABCOMP3PC10>
2025-02-27 18:28:42 -0600

    Este es un proyecto MVC

---
+++ Parcial_1/Controllers/CalculoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Parcial_1.Controllers
+{
+    public class CalculoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
